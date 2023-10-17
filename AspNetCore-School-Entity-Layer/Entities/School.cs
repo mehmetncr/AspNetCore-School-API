8ebc69b88ae7b5041696dddd89f348f026443812
mehmetncr <mehmet.ncar@gmail.com>
2023-10-18 01:31:12 +0300

    Add project files.

---
+++ AspNetCore-School-Entity-Layer/Entities/School.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCore_School_Entity_Layer.Entities
+{
+    public class School
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string District { get; set; }
+
+        public List<Class> Classes { get; set; }
+    }
+}
---
+++ AspNetCore-School-Entity-Layer/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCore_School_Entity_Layer.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string SchoolNo { get; set; }
+
+        public int ClassId { get; set; }
+        public Class Class { get; set; }
+    }
+}
